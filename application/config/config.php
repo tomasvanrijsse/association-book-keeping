1612d8e05f5cf36698bcc09db6c35fe0190d2b76
Tomas van Rijsse <tomas@vanrijsse.nl>
2020-02-01 21:26:17 +0100

    local setup

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://samenhuis-bills.dev/';
+$config['base_url']	= 'https://samenhuis-bills.test';
 
 /*
 |--------------------------------------------------------------------------
--- application/config/database.php
+++ application/config/database.php
@@ -49,10 +49,10 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'samenhuis_bills';
-$db['default']['password'] = 'secret';
-$db['default']['database'] = 'samenhuis_bills';
-$db['default']['dbdriver'] = 'mysql';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '';
+$db['default']['database'] = 'samenhuis-bills';
+$db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
