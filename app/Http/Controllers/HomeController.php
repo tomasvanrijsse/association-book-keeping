44b5aca836d3bd66b6c52b2ba04c77c108d661a0
Tomas van Rijsse <t.vanrijsse@intermax.nl>
2024-09-28 11:58:49 +0200

    chore: remove counter suggestions

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,19 +10,11 @@ class HomeController extends Controller {
 
     public function index() {
         $data = array(
-            'amountOfCounterSuggestions' => 0, //CounterSuggestion::query()->where('status', 1)->count(),
             'lastImport' => Setting::query()->where('key','LAST_IMPORT')->value('value'),
             'lastTransaction' => BankTransaction::query()->max('date')
         );
 
         return view('home/index', $data);
-    }
-
-    public function rescanCounter(){
-        //find new counter transactions
-        CounterSuggestion::findCounterTransactions();
-
-        return redirect('/home');
     }
 
 }
